eac37d0ffd1e0daf142d094aa0e0c308168181ea
Graver75 <undergraver75@gmail.com>
2020-02-07 09:07:37 +0300

    update user api

--- Backend/API/employees/create.php
+++ Backend/API/employees/create.php
@@ -5,6 +5,7 @@ include_once '../config/database.php';
 include_once '../models/Employee.php';
 include_once '../models/Skill.php';
 include_once '../models/Emp_Skl.php';
+include_once '../utils/Common.php';
 
 // db connection
 $database = new Database();
@@ -15,13 +16,21 @@ $employee = new Employee($db);
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
+if (Common::is_employee_id_set($_POST)) {
+    $employee->employee_id = $_POST['employee_id'];
+}
+
 $employee->first_name = $_POST['first_name'];
 $employee->last_name = $_POST['last_name'];
 $employee->age = $_POST['age'];
 $employee->exp = $_POST['age'];
 $skills = $_POST['skills'];
 
-$employee->create();
+if (Common::is_employee_id_set($_POST)) {
+    $employee->update();
+} else {
+    $employee->create();
+}
 
 foreach ($skills as $skill_raw) {
 
--- Backend/API/models/Employee.php
+++ Backend/API/models/Employee.php
@@ -18,11 +18,11 @@ class Employee {
 
     // all
     function query() {
-        $query = 'SELECT first_name, last_name, age, exp, GROUP_CONCAT(skill SEPARATOR ", ") AS skills
+        $query = 'SELECT employee_id, first_name, last_name, age, exp, GROUP_CONCAT(skill SEPARATOR ", ") AS skills
                   FROM emps_skls
                   JOIN employees USING (employee_id)
                   JOIN skills USING (skill_id)
-                  GROUP BY first_name, last_name, age, exp';
+                  GROUP BY employee_id, first_name, last_name, age, exp';
 
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -32,7 +32,7 @@ class Employee {
 
     // by field
     function query_by_field($field, $value) {
-        $query = 'SELECT first_name, last_name, age, exp, GROUP_CONCAT(skill SEPARATOR ", ") AS skills
+        $query = 'SELECT employee_id, first_name, last_name, age, exp, GROUP_CONCAT(skill SEPARATOR ", ") AS skills
                   FROM emps_skls
                   JOIN employees USING (employee_id)
                   JOIN skills USING (skill_id)
@@ -51,6 +51,16 @@ class Employee {
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$this->first_name, $this->last_name, $this->age, $this->exp]);
         $this->employee_id = $this->conn->lastInsertId();
+
+        return $stmt;
+    }
+
+    function update() {
+        $query = 'UPDATE employees SET first_name = ?, last_name = ?, age = ? , exp = ?
+                  WHERE employee_id = ? ';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$this->first_name, $this->last_name, $this->age, $this->exp, $this->employee_id]);
 
         return $stmt;
     }
